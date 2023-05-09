b1225c5759f3bf01be9950038c94ed0a8c1ad901
Shroomander <adriano.halo@gmail.com>
2023-05-09 19:56:43 +0100

    added more tests

--- test/betelgeuse/util/edn_test.clj
+++ test/betelgeuse/util/edn_test.clj
@@ -2,6 +2,14 @@
   (:require [clojure.test :refer :all] 
             [betelgeuse.util.edn :as edn]))
 
+(deftest passes-if-does-not-contain-necessary-keys-test
+  (testing "should check if failes with expected exception data" 
+    (try   
+      (edn/str-2-obj "{:a 1}" [:b :c])    
+      (catch Exception e  
+        (let [data (ex-data e)]
+          (is data {:object {:a 1} :keys [:b :c]}))))))
+
 (deftest test-passes-if-has-keys
   (testing "should check if object has the expected keys"
     (let [obj {:a 1 :b 2 :c 3}]
